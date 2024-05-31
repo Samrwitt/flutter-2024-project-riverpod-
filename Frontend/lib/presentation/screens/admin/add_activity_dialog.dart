f2e27e37bd5671d1a6f16264d8e291db1d3c1143
Samrwitt <samrawitkta@gmail.com>
2024-05-31 23:28:19 +0300

    final fetch and tests

--- Frontend/lib/presentation/screens/admin/add_activity_dialog.dart
+++ Frontend/lib/presentation/screens/admin/add_activity_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:go_router/go_router.dart';//use navigation go router
 
 class AddActivityDialog extends StatelessWidget {
   final TextEditingController userController;
